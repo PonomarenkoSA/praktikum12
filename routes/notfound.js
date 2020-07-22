bc0042fccc182614ee809896b53f5d8302bf72ba
Sergey Ponomarenko <lambo89s@yandex.ru>
2020-07-22 22:40:53 +0400

    Final commit of the 12th project

--- routes/notfound.js
+++ routes/notfound.js
@@ -1,22 +1,6 @@
 const notFoundRouter = require('express').Router();
 
-notFoundRouter.get('/', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-notFoundRouter.delete('/', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-notFoundRouter.post('/', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-notFoundRouter.put('/', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-notFoundRouter.patch('/', (req, res) => {
+notFoundRouter.all('/', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
